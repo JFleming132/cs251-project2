6babc5d3de8acb68e192c9cad042bf3880067fcb
JFleming132 <josephflemingvi@gmail.com>
2023-10-07 02:09:03 -0400

    Tested some and repaired functions

--- src/FancyBlockChain.java
+++ src/FancyBlockChain.java
@@ -2,23 +2,22 @@ public class FancyBlockChain {
     public Block[] bchain;
     public int length;
 
+    public int capacity;
     public int latestTimestamp;
     public FancyBlockChain(int capacity) {
         latestTimestamp = 0;
+        this.capacity = capacity;
         this.bchain = new Block[capacity];
         this.length = 0;
     }
     public FancyBlockChain(Block[] initialBlocks) {
         latestTimestamp = 0;
-        this.length = initialBlocks.length;
-        this.bchain = new Block[length];
-        for (int i = 0; i < length; i++) {
-            bchain[i] = initialBlocks[i];
-            if (bchain[i].timestamp > latestTimestamp) {
-                latestTimestamp = bchain[i].timestamp;
-            }
+        capacity = initialBlocks.length;
+        this.bchain = new Block[capacity];
+        for (Block initialBlock : initialBlocks) {
+            addBlock(initialBlock);
         }
-        makeMinHeap();
+        print();
     }
 
     public int length() {
@@ -26,26 +25,25 @@ public class FancyBlockChain {
     }
 
     public boolean addBlock(Block newBlock) {
-        if (this.length() < this.bchain.length) { //newblock at end
-            bchain[length()] = newBlock;
+        //print();
+        //System.out.println("Adding block " + newBlock.timestamp);
+        if ((length() == capacity) && (bchain[0].timestamp < newBlock.timestamp)) {
+            removeEarliestBlock();
+        }
+        else if (length == capacity) {
+            return false;
+        }
+        if (this.length() < capacity) {
+            if (newBlock.timestamp > latestTimestamp) {
+                latestTimestamp = newBlock.timestamp;
+            }
             newBlock.index = length();
+            this.bchain[length()] = newBlock;
+            length++;
             makeMinHeap();
-            if (newBlock.timestamp > latestTimestamp) {
-                latestTimestamp = newBlock.timestamp;
-            }
-            return true;
+            //print();
         }
-        else if (this.bchain[0].timestamp < newBlock.timestamp) { //newblock at start
-            this.bchain[0].removed = true;
-            this.bchain[0] = newBlock;
-            newBlock.index = 0;
-            makeMinHeap();
-            if (newBlock.timestamp > latestTimestamp) {
-                latestTimestamp = newBlock.timestamp;
-            }
-            return true;
-        }
-        return false;
+        return true;
     }
     public Block getEarliestBlock() {
         if (length() > 0) {
@@ -62,6 +60,7 @@ public class FancyBlockChain {
         return null;
     }
     public Block removeEarliestBlock() {
+        //print();
         Block returnBlock = null;
         if (length == 1) {
             length--;
@@ -71,26 +70,32 @@ public class FancyBlockChain {
         }
         else if (length() > 1) {
             returnBlock = bchain[0];
-            bchain[0] = bchain[length()];
-            bchain[length()] = null;
+            bchain[0] = bchain[length() - 1];
+            bchain[length() - 1] = null;
             length--;
             returnBlock.removed = true;
             bchain[0].index = 0;
             makeMinHeap();
         }
+        //print();
         return returnBlock;
     }
     public Block removeBlock(String data) {
+        //print();
+        //System.out.println("removing block w data" + data);
         Block returnBlock = getBlock(data);
         if (returnBlock == null) {
+            //System.out.println("no such block");
             return null;
         } else {
-            bchain[returnBlock.index] = bchain[length()];
-            bchain[length()] = null;
+            if (returnBlock.index != (length() - 1)) {
+                bchain[returnBlock.index] = bchain[length() - 1];
+            }
+            bchain[length() - 1] = null;
             length--;
             returnBlock.removed = true;
-            bchain[returnBlock.index].index = returnBlock.index;
             makeMinHeap();
+            //print();
             return returnBlock;
         }
     }
@@ -98,6 +103,9 @@ public class FancyBlockChain {
         bchain[0].nonce = nonce;
         latestTimestamp++;
         bchain[0].timestamp = latestTimestamp;
+        Block tempBlock = bchain[length() - 1];
+        bchain[length() - 1] = bchain[0];
+        bchain[0] = tempBlock;
         makeMinHeap();
     }
     public void updateBlock(String data, double nonce) {
@@ -116,19 +124,28 @@ public class FancyBlockChain {
         }
     }
     public void heapify(int i) { //helper function to sink down max elements
-        int largest = i;
-        if (((2 * i) + 1) < length() && (bchain[i].timestamp > bchain[(i * 2) + 1].timestamp)) {
-            largest = (2 * i) + 1;
+        int minimum = i;
+        if (((2 * i) + 1) < length() && (bchain[minimum].timestamp > bchain[(i * 2) + 1].timestamp)) {
+            minimum = (2 * i) + 1;
         }
-        if (((2 * i) + 2) < length() && (bchain[i].timestamp > bchain[(i * 2) + 2].timestamp)) {
-            largest = (2 * i) + 2;
+        if (((2 * i) + 2) < length() && (bchain[minimum].timestamp > bchain[(i * 2) + 2].timestamp)) {
+            minimum = (2 * i) + 2;
         }
-        if (largest != i) {
+        if (minimum != i) {
             Block tempBlock = bchain[i];
-            bchain[i] = bchain[largest];
-            bchain[largest] = tempBlock;
+            bchain[i] = bchain[minimum];
+            bchain[minimum] = tempBlock;
             bchain[i].index = i;
-            bchain[largest].index = largest;
+            bchain[minimum].index = minimum;
+            heapify(minimum);
         }
+    }
+
+    public void print() {
+        System.out.print("The heap has length " + length() + ", capacity " + capacity + ", max timestamp is " + latestTimestamp + ", and is: ");
+        for (int i = 0; i < length(); i++) {
+            System.out.print(bchain[i].timestamp + " ");
+        }
+        System.out.println();
     }
 }
