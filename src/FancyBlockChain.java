66422b0f234a149f9fd36a3339ab1966fa298fee
JFleming132 <josephflemingvi@gmail.com>
2023-10-07 00:32:49 -0400

    Finished these functions, currently untested though

--- src/FancyBlockChain.java
+++ src/FancyBlockChain.java
@@ -1,72 +1,37 @@
 public class FancyBlockChain {
     public Block[] bchain;
     public int length;
+
+    public int latestTimestamp;
     public FancyBlockChain(int capacity) {
+        latestTimestamp = 0;
         this.bchain = new Block[capacity];
         this.length = 0;
     }
     public FancyBlockChain(Block[] initialBlocks) {
-        int lastNonLeaf = (initialBlocks.length / 2) - 1;
-        int currIndex = 0;
-        this.length = 0;
-        int leftChildIndex = 0;
-        int rightChildIndex = 0;
-        Block tempBlock = null;
-        for (int i = lastNonLeaf; i >= 0; i--) {
-            currIndex = i;
-            while (true) {
-                leftChildIndex = (currIndex * 2) + 1;
-                rightChildIndex = (currIndex * 2) + 2;
-                if ((initialBlocks.length > rightChildIndex)
-                        && (initialBlocks[rightChildIndex].timestamp < initialBlocks[currIndex].timestamp)) {
-                    tempBlock = initialBlocks[currIndex];
-                    initialBlocks[currIndex] = initialBlocks[rightChildIndex];
-                    initialBlocks[rightChildIndex] = tempBlock;
-                    currIndex = rightChildIndex;
-                } else if ((initialBlocks.length > leftChildIndex)
-                        && (initialBlocks[leftChildIndex].timestamp < initialBlocks[currIndex].timestamp)) {
-                    tempBlock = initialBlocks[currIndex];
-                    initialBlocks[currIndex] = initialBlocks[leftChildIndex];
-                    initialBlocks[leftChildIndex] = tempBlock;
-                    currIndex = leftChildIndex;
-                }
-                else {
-                    break;
-                }
+        latestTimestamp = 0;
+        this.length = initialBlocks.length;
+        this.bchain = new Block[length];
+        for (int i = 0; i < length; i++) {
+            bchain[i] = initialBlocks[i];
+            if (bchain[i].timestamp > latestTimestamp) {
+                latestTimestamp = bchain[i].timestamp;
             }
         }
-        this.bchain = new Block[initialBlocks.length];
-        for (int j = 0; j < initialBlocks.length; j++) {
-            this.bchain[j] = initialBlocks[j];
-            this.bchain[j].index = j;
-            this.length++;
-        }
+        makeMinHeap();
     }
 
     public int length() {
-
         return this.length;
     }
 
     public boolean addBlock(Block newBlock) {
-        int currIndex = 0;
         if (this.length() < this.bchain.length) { //newblock at end
-            newBlock.index = this.length();
-            this.bchain[this.length()] = newBlock;
-            currIndex = this.length();
-            Block tempBlock = null;
-            this.length++;
-            while (currIndex != 0) {
-                if (this.bchain[(currIndex - 1) / 2].timestamp > this.bchain[currIndex].timestamp) {
-                    tempBlock = this.bchain[currIndex];
-                    this.bchain[currIndex] = this.bchain[(currIndex - 1) / 2];
-                    this.bchain[currIndex].index = (currIndex - 1) / 2;
-                    this.bchain[(currIndex - 1) / 2] = tempBlock;
-                    this.bchain[(currIndex - 1) / 2].index = currIndex;
-                    currIndex = (currIndex - 1) / 2;
-                } else {
-                    break;
-                }
+            bchain[length()] = newBlock;
+            newBlock.index = length();
+            makeMinHeap();
+            if (newBlock.timestamp > latestTimestamp) {
+                latestTimestamp = newBlock.timestamp;
             }
             return true;
         }
@@ -74,53 +39,96 @@ public class FancyBlockChain {
             this.bchain[0].removed = true;
             this.bchain[0] = newBlock;
             newBlock.index = 0;
-            Block tempBlock = null;
-            int leftchildindex = 1;
-            int rightchildindex = 2;
-            while (leftchildindex < this.length()) { //while currIndex has left kid, i.e. not end of list
-                if ((rightchildindex < this.length()) && //right child exists
-                        (this.bchain[rightchildindex].timestamp < this.bchain[leftchildindex].timestamp) && //right key is less than left key
-                        (this.bchain[currIndex].timestamp > this.bchain[rightchildindex].timestamp)) { //currIndex key is more than right key
-                    //swap currIndex and right node, update currIndex
-                    tempBlock = this.bchain[rightchildindex];
-                    this.bchain[rightchildindex] = this.bchain[currIndex];
-                    this.bchain[currIndex] = tempBlock;
-                    this.bchain[currIndex].index = rightchildindex; //update index
-                    this.bchain[rightchildindex].index = currIndex; //update index
-                    currIndex = rightchildindex;
-                } else if (this.bchain[currIndex].timestamp > this.bchain[leftchildindex].timestamp) { //currIndex key is greater than left key
-                    //swap currIndex and left node, update currIndex
-                    tempBlock = this.bchain[leftchildindex];
-                    this.bchain[leftchildindex] = this.bchain[currIndex];
-                    this.bchain[currIndex] = tempBlock;
-                    this.bchain[currIndex].index = leftchildindex;
-                    this.bchain[leftchildindex].index = currIndex;
-                    currIndex = leftchildindex;
-                }
-                else { //node is in right place
-                    break;
-                }
-                leftchildindex = (2 * currIndex) + 1; //update child pointers
-                rightchildindex = (2 * currIndex) + 2;
+            makeMinHeap();
+            if (newBlock.timestamp > latestTimestamp) {
+                latestTimestamp = newBlock.timestamp;
             }
             return true;
         }
         return false;
     }
     public Block getEarliestBlock() {
+        if (length() > 0) {
+            return bchain[0];
+        }
         return null;
     }
     public Block getBlock(String data) {
+        for (int i = 0; i < length(); i++) {
+            if (bchain[i].data.equals(data)) {
+                return bchain[i];
+            }
+        }
         return null;
     }
     public Block removeEarliestBlock() {
-        return null;
+        Block returnBlock = null;
+        if (length == 1) {
+            length--;
+            returnBlock = bchain[0];
+            bchain[0] = null;
+            returnBlock.removed = true;
+        }
+        else if (length() > 1) {
+            returnBlock = bchain[0];
+            bchain[0] = bchain[length()];
+            bchain[length()] = null;
+            length--;
+            returnBlock.removed = true;
+            bchain[0].index = 0;
+            makeMinHeap();
+        }
+        return returnBlock;
     }
     public Block removeBlock(String data) {
-        return null;
+        Block returnBlock = getBlock(data);
+        if (returnBlock == null) {
+            return null;
+        } else {
+            bchain[returnBlock.index] = bchain[length()];
+            bchain[length()] = null;
+            length--;
+            returnBlock.removed = true;
+            bchain[returnBlock.index].index = returnBlock.index;
+            makeMinHeap();
+            return returnBlock;
+        }
     }
     public void updateEarliestBlock(double nonce) {
+        bchain[0].nonce = nonce;
+        latestTimestamp++;
+        bchain[0].timestamp = latestTimestamp;
+        makeMinHeap();
     }
     public void updateBlock(String data, double nonce) {
+        Block currentBlock = getBlock(data);
+        if (currentBlock != null) {
+            currentBlock.nonce = nonce;
+            latestTimestamp++;
+            currentBlock.timestamp = latestTimestamp;
+            makeMinHeap();
+        }
+    }
+
+    public void makeMinHeap() { //turns the current bchain into a minHeap
+        for (int i = (length() - 1) / 2; i >= 0; i--) {
+            heapify(i);
+        }
+    }
+    public void heapify(int i) { //helper function to sink down max elements
+        int largest = i;
+        if (((2 * i) + 1) < length() && (bchain[i].timestamp > bchain[(i * 2) + 1].timestamp)) {
+            largest = (2 * i) + 1;
+        }
+        if (((2 * i) + 2) < length() && (bchain[i].timestamp > bchain[(i * 2) + 2].timestamp)) {
+            largest = (2 * i) + 2;
+        }
+        if (largest != i) {
+            Block tempBlock = bchain[i];
+            bchain[i] = bchain[largest];
+            bchain[largest] = tempBlock;
+            bchain[i].index = i;
+            bchain[largest].index = largest;
+        }
     }
 }
